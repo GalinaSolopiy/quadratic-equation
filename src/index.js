79ac5e005525c13758157b74b021ab63583f6e01
GalinaSolopiy <sharicbo@gmail.com>
2018-02-26 16:02:31 +0300

    true story

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function solveEquation(equation) {
-  // your implementation
+module.exports = function solveEquation(equation){
+    let expression = equation.match(/(-?\d+)(?:\s\*\sx\^2\s)([-,+]\s\d+)(?:\s\*\sx\s)([-,+]\s\d+)/);
+    let a = +expression[1];
+    let b = +expression[2].replace(' ', '');
+    let c = +expression[3].replace(' ', '');
+    let D = Math.sqrt(b*b - 4*a*c);
+    let x1 = Math.round((-b + D)/(2*a));
+    let x2 = Math.round((-b - D)/(2*a));
+    return x2 > x1 ? [x1, x2] : [x2, x1];
 }
+
+
+
